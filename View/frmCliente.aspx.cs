04cbff9e6a5c25db4a8d3a89ada626e19c62cc30
Mike Cardona <mikecardona076@gmail.com>
2020-07-13 07:40:27 -0700

    1.3

--- View/frmCliente.aspx.cs
+++ View/frmCliente.aspx.cs
@@ -13,16 +13,22 @@ namespace Facturas_proyecto.View
     public partial class WebForm1 : System.Web.UI.Page
     {
         #region properties
+        //.................................................................................................//
+
         public List<Cliente> ClienteList
         {
             get;
             set;
         }
+
+        //.................................................................................................//
+
         #endregion
 
         #region Methods
         #endregion
 
+        //.................................................................................................//
         public Int32 InsertarCliente(Cliente cte)
         {
             Int32 affectedRows = 0;
@@ -40,6 +46,9 @@ namespace Facturas_proyecto.View
 
         }
 
+        //.................................................................................................//
+
+
         public Int32 SeleccionarCliente(Cliente cte)
         {
             Int32 affectedRows = 0;
@@ -48,9 +57,16 @@ namespace Facturas_proyecto.View
             {
                 affectedRows = ClienteDAL.SeleccionarCliente(cte);
             }
+                        catch (Exception ex)
+            {
+
+            }
             return;
 
         }
+
+        //.................................................................................................//
+
         public Int32 ActualizarCliente(Cliente cte)
         {
             Int32 affectedRows = 0;
@@ -66,17 +82,27 @@ namespace Facturas_proyecto.View
             return affectedRows;
 
         }
+
+        //.................................................................................................//
+
         public Int32 BorrarCliente(Cliente cte)
         {
             Int32 affectedRows = 0;
 
             try
             {
-                affectedRows =
+                affectedRows = ClienteDAL.BorrarCliente(cte);
             }
-            return;
+            catch (Exception ex)
+            {
+
+            }
+            return affectedRows;
 
         }
+
+        //.................................................................................................//
+
 
         public void RellenarGridClientes(List<Cliente> ClienteList)
         {
@@ -84,13 +110,15 @@ namespace Facturas_proyecto.View
             {
                 GridClientes.DataSource = ClienteList;
                 GridClientes.DataBind();
-
             }
             catch(Exception ex)
             {
 
             }
         }
+
+        //.................................................................................................//
+
         
         //Events
         protected void Page_Load(object sender, EventArgs e)
@@ -102,20 +130,54 @@ namespace Facturas_proyecto.View
                 String Rfc = "";
 
                 ClienteList = new List<Cliente>();
-                ClienteList = SeleccionarCliente(IdCte);
+                ClienteList = SeleccionarCliente(IdCte, Nombre, Rfc);
+                RellenarGridClientes(ClienteList);
+
+            }
+            catch(Exception ex){
+
             }
 
         }
 
+        //.................................................................................................//
+
+
         protected void btnInsertar_Click(object sender, EventArgs e)
         {
+            try{
+                ClienteList cte = new Cliente();
+
+                cte.Nombre = txbNombre.Text;
+                cte.Direccion = txbDireccion.Text;
+                cte.Telefono = txbTelefono.Text;
+                cte.Rfc = txbRfc.Text;
+                cte.Email = txbEmail.Text;
+                cte.FechaNacimieno = Convert.ToDateTime(txbFechaNacimiento);
+
+                Int32 affectedRows = 0;
+
+                affectedRows = InsertarCliente(cte);
+                if(affectedRows > 0){
+                    //Mandar mensaje
+                }
+            }
+            catch(Exception ex){
+
+            }
 
         }
+
+        //.................................................................................................//
+
 
         protected void btnConsultar_Click(object sender, EventArgs e)
         {
 
         }
+
+        //.................................................................................................//
+
 
         protected void btnLimpiar_Click(object sender, EventArgs e)
         {
--- View/frmProducto.aspx.cs
+++ View/frmProducto.aspx.cs
@@ -4,14 +4,175 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Facturas_proyecto.BussinesObjects;
 
 namespace Facturas_proyecto.View
 {
     public partial class frmProducto : System.Web.UI.Page
     {
+        #region properties
+        public List<Producto> ProductoList
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Methods
+        #endregion
+        //.................................................................................................//
+        public Int32 InsertarProducto(Producto prd)
+        {
+            Int32 affectedRows = 0;
+
+            try
+            {
+                affectedRows = ProductoDAL.InsertarProducto();
+            }
+            catch(Exception ex)
+            {
+
+            }
+
+            return affectedRows;
+
+        }
+
+        //.................................................................................................//
+
+        public Int32 SeleccionarProducto(Cliente cte)
+        {
+            Int32 affectedRows = 0;
+
+            try
+            {
+                affectedRows = ProductoDAL.SeleccionarProducto(prod);
+            }
+                        catch (Exception ex)
+            {
+
+            }
+            return;
+
+        }
+
+        //.................................................................................................//
+
+        public Int32 ActualizarProducto(Cliente prod)
+        {
+            Int32 affectedRows = 0;
+
+            try
+            {
+                affectedRows = ProductoDAL.ActualizarProducto(prod);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return affectedRows;
+
+        }
+
+        //.................................................................................................//
+
+        public Int32 BorrarProducto(Cliente cte)
+        {
+            Int32 affectedRows = 0;
+
+            try
+            {
+                affectedRows = ProductoDAL.BorrarProducto(prod);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return affectedRows;
+
+        }
+
+        //.................................................................................................//
+
+
+        public void RellenarGridProducto(List<Producto> ProductoList)
+        {
+            try
+            {
+                GridProducto.DataSource = ProductoList;
+                GridProducto.DataBind();
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        //.................................................................................................//
+
+        
+        //Events
         protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                Int32? Idprod = null;
+                String Descripcion = "";
+                Int32? CodigoBarras = "";
+
+                ProductoList = new List<Producto>();
+                ProductoList = SeleccionarProducto(Idprod, Descripcion, CodigoBarras);
+                RellenarGridProducto(ProductoList);
+
+            }
+            catch(Exception ex){
+
+            }
+
+        }
+
+        //.................................................................................................//
+        protected void btnInsertar_Click(object sender, EventArgs e)
+        {
+            try{
+                ProductoList prod = new Producto();
+
+                prod.Descripcion = txbDescripcion.Text;
+                prod.Precio = txbPrecio.Text;
+                prod.FechaCaducidad = Convert.ToDateTime(txbFechaCaducidad);
+                prod.CodigoBarras = txbCodigoBarras.Text;
+                prod.ProveedorProd = txbProveedorProd.Text;
+
+                Int32 affectedRows = 0;
+
+                affectedRows = InsertarProducto(prod);
+                if(affectedRows > 0){
+                    //Mandar mensaje
+                }
+            }
+            catch(Exception ex){
+
+            }
+
+        }
+
+        //.................................................................................................//
+
+
+        protected void btnConsultar_Click(object sender, EventArgs e)
         {
 
         }
+
+        //.................................................................................................//
+
+
+        protected void btnLimpiar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //.................................................................................................//
+
     }
 }
